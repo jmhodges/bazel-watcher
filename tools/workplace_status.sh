a03ea780db60b13a1cfb7585c044b750d20bc83f
Jeff Hodges <jeff@somethingsimilar.com>
2018-02-10 16:07:07 -0800

    hack

--- tools/workplace_status.sh
+++ tools/workplace_status.sh
@@ -1,15 +1,13 @@
 #! /usr/bin/env bash
 
-# A hack to make the `git describe` work correctly in Travis CI.
-if [[ $TRAVIS == "true" ]]; then
-  echo "GIT FETCHING"
-  git fetch  --tags
-else
-  echo "NOPE ${TRAVIS}"
-fi
-
 
 echo -n "STABLE_GIT_VERSION "
+# A hack to make the `git describe` work correctly in Travis CI.
+if [[ $TRAVIS == "true" ]]; then
+  printf "%s-dirty\n" "$(git rev-parse HEAD)"
+  exit
+fi
+
 if git diff-index --quiet HEAD -- > /dev/null 2>&1; then
   git describe --tags --abbrev=0
 else
